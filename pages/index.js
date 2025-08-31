a67ecf835cca20c79b8128714043b2299e2533df
Ajay Kumar <aalaboina@ruggedmonitoring.com>
2025-08-31 15:48:39 +0530

    COMMIT3

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1> Hello Welcome to Azure!</h1>
+      <h1> Next JS App demo!</h1>
       <p>This is a dummy Next.js app deployed with GitHub Actions.</p>
     </div>
   );
