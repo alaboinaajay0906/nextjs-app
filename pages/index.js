3a21cdf1ad364878f1bccf467f96c3ebf07510f2
Ajay Kumar <aalaboina@ruggedmonitoring.com>
2025-08-31 12:32:40 +0530

    updated Hello to Sindhuja

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello Azure 🚀</h1>
+      <h1>Hello sindhuja 🚀</h1>
       <p>This is a dummy Next.js app deployed with GitHub Actions.</p>
     </div>
   );
