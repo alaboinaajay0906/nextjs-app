4621c3993b492b7fd2eb27b83a9a1154a61a386e
Ajay Kumar <aalaboina@ruggedmonitoring.com>
2025-09-02 20:48:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1> Next JS App Live Final!</h1>
+      <h1> Next JS App Live Final demo!</h1>
       <p>This is a dummy Next.js app deployed with GitHub Actions.</p>
     </div>
   );
